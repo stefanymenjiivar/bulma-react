7952308972ad6d54b031cb0a6053485cd8e898a3
kernel503 <juanko682@gmail.com>
2021-01-11 17:54:21 -0600

    Initial Commit 🍰🍰

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React, { useCallback, useState } from 'react';
+import Navbar from './Component/Navbar';
+
+const menuOptions = ['Agregar incapacidad', 'Gestionar empleados'];
 
 function App() {
+  const [optionSelected, setOptionSelected] = useState(-1);
+
+  const setActiveOption = useCallback((key) => (event) => setOptionSelected(key), []);
+
+  const menu = menuOptions.map((text, key) => {
+    let classMenu = '';
+    if (key === optionSelected) {
+      classMenu = 'is-active';
+    }
+    return (
+      <li key={key} onClick={setActiveOption(key)}>
+        <a className={classMenu}>{text}</a>
+      </li>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className='columns p-5'>
+        <div className='column is-2'>
+          <aside className='menu'>
+            <p className='menu-label'>Gestionar</p>
+            <ul className='menu-list'>{menu}</ul>
+          </aside>
+        </div>
+        <div className='column'>
+          <section className='section'>
+            <div className='container'>
+              <table className='table is-fullwidth is-hoverable'>
+                <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Code</th>
+                    <th>Start Date</th>
+                    <th>DUI</th>
+                    <th>Position</th>
+                    <th>Config</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>19</th>
+                    <td>38</td>
+                    <td>9</td>
+                    <td>7</td>
+                    <td>22</td>
+                    <td>
+                      <p className='buttons'>
+                        <button className='button is-small is-info is-outlined'>
+                          <span className='icon is-small'>
+                            <i className='fas fa-user-edit' />
+                          </span>
+                        </button>
+                        <button className='button is-small is-danger is-outlined'>
+                          <span className='icon is-small'>
+                            <i className='fas fa-user-times' />
+                          </span>
+                        </button>
+                      </p>
+                    </td>
+                  </tr>
+                  <tr />
+                </tbody>
+              </table>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
 
